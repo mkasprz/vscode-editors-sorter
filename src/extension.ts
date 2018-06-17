9e1f9d38ff1aaa39aedee7c7eec5c0dfcd8c09f3
K <kasprzyk@protonmail.com>
2018-06-17 22:53:37 +0200

    Better support of non-text editors - they shall be moved to the end

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 import {TextDocument, TextEditor} from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    async function getActiveTextEditor(currentTextEditor?: TextEditor, timeoutBetweenChecks = 100, maximumTotalTimeout = 5000): Promise<TextEditor | undefined> {
+    async function getActiveTextEditor(currentTextEditor?: TextEditor, timeoutBetweenChecks = 100, maximumTotalTimeout = 3000): Promise<TextEditor | undefined> {
         // [TODO] I do believe it can be done better.
         let activeTextEditor = vscode.window.activeTextEditor;
         for (let iteration = 0; (!activeTextEditor || activeTextEditor == currentTextEditor) && iteration < maximumTotalTimeout / 100; iteration++) {
@@ -13,13 +13,15 @@ export function activate(context: vscode.ExtensionContext) {
         return activeTextEditor;
     }
 
+    async function getNextTextEditor(currentTextEditor?: TextEditor) {
+        await vscode.commands.executeCommand('workbench.action.nextEditor');
+        return await getActiveTextEditor(currentTextEditor);
+    }
+
     async function getNextDefinedTextEditor(iterationsLimit = 100): Promise<TextEditor | undefined> {
-        let currentTextEditor = vscode.window.activeTextEditor;
-        let activeTextEditor: TextEditor | undefined
-        for (let iteration = 0; !activeTextEditor && iteration < iterationsLimit; iteration++) {
-            await vscode.commands.executeCommand('workbench.action.nextEditor');
-            activeTextEditor = await getActiveTextEditor(currentTextEditor);
-            currentTextEditor = activeTextEditor;
+        let activeTextEditor = vscode.window.activeTextEditor;
+        for (let iteration = 0; !iteration || !activeTextEditor && iteration < iterationsLimit; iteration++) {
+            activeTextEditor = await getNextTextEditor(activeTextEditor);
         }
         return activeTextEditor;
     }
@@ -27,36 +29,51 @@ export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('editors-sorter.sortEditors', async () => {
         const firstTextEditor = vscode.window.activeTextEditor || await getNextDefinedTextEditor();
         if (firstTextEditor) {
+            await vscode.commands.executeCommand('moveActiveEditor', {by: 'tab', to: 'position', value: 2});
             const firstTextDocument = firstTextEditor.document;
             let textDocuments: TextDocument[] = [firstTextDocument];
-            let activeTextDocument: TextDocument | undefined = firstTextDocument;
+            let numberOfNonTextEditors = 0;
+            let activeTextDocument: TextDocument | undefined;
+            let activeTextEditor: TextEditor | undefined = firstTextEditor;
             do {
-                let activeTextEditor = await getNextDefinedTextEditor();
+                activeTextEditor = await getNextTextEditor(activeTextEditor);
                 if (activeTextEditor) {
                     activeTextDocument = activeTextEditor.document;
                     if (activeTextDocument != firstTextDocument) {
                         textDocuments.push(activeTextDocument);
                     }
+                } else {
+                    numberOfNonTextEditors++;
                 }
             } while (activeTextDocument != firstTextDocument)
 
-            let filesNames = textDocuments.map(textDocument => {
+            const filesNames = textDocuments.map(textDocument => {
                 return textDocument.fileName.toLowerCase();
             });
+            const numberOfTextEditors = filesNames.length;
             filesNames.sort();
+            let sortedNonTextEditors = 0;
 
             do {
                 if (activeTextDocument) {
                     const indexOfTextDocument = textDocuments.indexOf(activeTextDocument);
                     if (indexOfTextDocument != -1) {
+                        const newPosition = filesNames.indexOf(activeTextDocument.fileName.toLowerCase()) + 1;
                         await vscode.commands.executeCommand('moveActiveEditor',
-                            {by: 'tab', to: 'position', value: filesNames.indexOf(activeTextDocument.fileName.toLowerCase()) + 1});
+                            {by: 'tab', to: 'position', value: newPosition});
                         textDocuments.splice(indexOfTextDocument, 1);
+                        if (newPosition == numberOfTextEditors && sortedNonTextEditors) {
+                            sortedNonTextEditors--;
+                        }
                     }
+                } else {
+                    await vscode.commands.executeCommand('moveActiveEditor',
+                        {by: 'tab', to: 'position', value: numberOfTextEditors + numberOfNonTextEditors});
+                    sortedNonTextEditors++;
                 }
-                const activeTextEditor = await getNextDefinedTextEditor();
+                const activeTextEditor = await getNextTextEditor(vscode.window.activeTextEditor);
                 activeTextDocument = activeTextEditor ? activeTextEditor.document : undefined;
-            } while (textDocuments.length)
+            } while (textDocuments.length && sortedNonTextEditors != numberOfNonTextEditors)
             vscode.window.showInformationMessage('Done sorting!');
         } else {
             vscode.window.showInformationMessage('Do You truly have at least 100 non-text editors opened? \
